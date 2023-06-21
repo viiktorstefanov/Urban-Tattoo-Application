8110dad7ece52b39793b14b6933af5b0974d4be4
viktor0110 <viktor-stefanov@mail.bg>
2023-06-21 20:48:56 +0300

    gallery page js added.

--- src/js/homePage.js
+++ src/js/homePage.js
@@ -1,8 +1,16 @@
-let contentElement = document.getElementById('content');
+const contentElement = document.getElementById('content');
+const galleryBtn = document.getElementById('gallery');
+
+import { galleryPage } from './galleryPage.js';
+
 contentElement.addEventListener('click', (e) => {
     if(e.currentTarget.style.color === 'white') {
         e.currentTarget.style.color = 'red';
     }else {
         e.currentTarget.style.color = 'white';
     }
-})
+});
+
+galleryBtn.addEventListener('click', (e) => {
+    galleryPage();
+} )
